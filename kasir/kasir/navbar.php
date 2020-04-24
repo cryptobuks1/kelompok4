dde57cf1ef7da812ce7d35b20d2f31fba61fc050
satriamaulana <55432210+satriamaulana@users.noreply.github.com>
2020-04-24 14:57:54 +0700

    satriamaulana

---
+++ kasir/kasir/navbar.php
@@ -0,0 +1,8 @@
+<div class="navigasi">
+  <a href="?page=home">Home</a>
+  <a href="?page=pesanan">Pesanan</a>
+  <a href="?page=transaksi">Transaksi</a>
+  <a href="?page=barang">Barang</a>
+  <a href="?page=pengguna">Pengguna</a>
+  
+</div>
