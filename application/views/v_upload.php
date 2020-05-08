c4ea6f1f765efa309836504ea632a89f4c5ed713
Ansory45agt <55432644+Ansory45agt@users.noreply.github.com>
2020-05-09 02:24:20 +0700

    upload file

---
+++ application/views/v_upload.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<title>Stationary Online</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/dropify/dropify.min.css'?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/style.css'?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap-grid.css'?>">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" "></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" "></script>
+	
+</head>
+<body >
+
+
+<nav class="navbar navbar-expand-lg navbar navbar-light" style="background-color: #00FFFF ">
+  <a class="navbar-brand" href="#">Stationary Online</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Percetakan
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Cetak Banner</a>
+          <a class="dropdown-item" href="#">Cetak Foto</a>
+          <a class="dropdown-item" href="#">Cetak Biasa</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Produk
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Alat Tulis</a>
+          <a class="dropdown-item" href="#">Elektronik</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Masuk</button>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Daftar</button>
+    </form>
+  </div>
+</nav> 
+
+
+
+
+	<div class="container">
+		<div class="col-sm-4 col-md-offset-4" style="margin:auto!important;">
+		<h4>Silahkan Unggah File Anda</h4>
+			<form class="form-horizontal" action="<?php echo base_url().'index.php/upload/upload_image'?>" method="post" enctype="multipart/form-data">
+			<div class="container">
+
+  <select class="form-control" placeholder="Silahkan Pilih">
+  <option>Cetak File</option>
+  <option>Cetak Banner</option>
+  <option>Cetak Foto</option>
+  
+</select>
+ 
+</select>
+
+</div>
+			
+			
+			<div class="form-group">
+					<input type="text" name="Deskripsi" class="form-control" placeholder="Deskripsi">
+				</div>
+				
+				<div class="form-group">
+					<input type="file" name="filefoto" class="dropify" data-height="300">
+				</div>
+				<div class="form-group">
+					<button class="btn btn-success" type="submit">Simpan</button>
+				</div>
+			</form>	
+		</div>
+	</div>
+<script type="text/javascript" src="<?php echo base_url().'assets/js/jquery.js'?>"></script>
+<script type="text/javascript" src="<?php echo base_url().'assets/js/bootstrap.js'?>"></script>
+<script type="text/javascript" src="<?php echo base_url().'assets/dropify/dropify.min.js'?>"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.dropify').dropify({
+			messages: {
+                default: 'Drag atau drop untuk memilih gambar',
+                replace: 'Ganti',
+                remove:  'Hapus',
+                error:   'error'
+            }
+		});
+	});
+	
+</script>
+
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4   footer-light" style="background-color: #000000  ">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Stationary Online</h6>
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          consectetur
+          adipisicing elit.</p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+        <p>
+          <a href="#!">Alat Tulis</a>
+        </p>
+        <p>
+          <a href="#!">Elektronik</a>
+        </p>
+        <p>
+          <a href="#!">Percetakan</a>
+        </p>
+        <p>
+          <a href="#!">Tapay Item</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+          <a href="#!">Your Account</a>
+        </p>
+        <p>
+          <a href="#!">Become an Affiliate</a>
+        </p>
+        <p>
+          <a href="#!">Shipping Rates</a>
+        </p>
+        <p>
+          <a href="#!">Help</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i> jember, SM 10012, INA</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> StationaryOnline@gmail.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> + 62 234 567 88</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 62 234 567 89</p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2020 Copyright:
+          <a href="/">
+            <strong> StationaryOnline.com</strong>
+          </a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-google-plus-g"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
+</body>
+</html>
